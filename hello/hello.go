95c05ac795d7d7e2b85fc4e13968563ffebf8ffd
unknown <philipp.keydel@web.de>
2021-03-10 18:17:04 +0100

    first iteration seems to be working

--- hello/hello.go
+++ hello/hello.go
@@ -18,17 +18,43 @@ package main
 
 import (
 	"bufio"
-	"crypto/rand"
 	"fmt"
+	"math/rand"
 	"os"
 	"strings"
+	"time"
 )
 
 func main() {
-	pw := "AB12cdEF34gh"
-	// fmt.Printf("Your password is %s. Have fun! :)\n", pw)
-	firstChar := rand.Int()
-	fmt.Printf("Your password's first character is an upper-case letter: %c (from %d%). Have fun! :)\n", firstChar, firstChar)
+	// pw := "AB12cdEF34gh"
+	lowerCaseLetters := "abcdefghijklmnopqrstuvwxyz"
+	// upperCaseLetters := "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	upperCaseLetters := "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+	// rand.Seed(seed int64)
+	rand.Seed(time.Now().UnixNano())
+
+	char1 := upperCaseLetters[rand.Intn(len(upperCaseLetters))]
+	char2 := upperCaseLetters[rand.Intn(len(upperCaseLetters))]
+	char3 := rand.Intn(9)
+	char4 := rand.Intn(9)
+	char5 := lowerCaseLetters[rand.Intn(len(lowerCaseLetters))]
+	char6 := lowerCaseLetters[rand.Intn(len(lowerCaseLetters))]
+	char7 := upperCaseLetters[rand.Intn(len(upperCaseLetters))]
+	char8 := upperCaseLetters[rand.Intn(len(upperCaseLetters))]
+	char9 := rand.Intn(9)
+	char10 := rand.Intn(9)
+	char11 := lowerCaseLetters[rand.Intn(len(lowerCaseLetters))]
+	char12 := lowerCaseLetters[rand.Intn(len(lowerCaseLetters))]
+
+	// fmt.Printf("Your password's first character is an upper-case letter: %c (from %d). Have fun! :)\n\n", firstChar, firstChar)
+	fmt.Printf("Your password is %c%c%d%d%c%c%c%c%d%d%c%c. Have fun! :)\n\n", char1, char2, char3, char4, char5, char6, char7, char8, char9, char10, char11, char12)
+
+	/*
+		for k, v := range s {
+			fmt.Printf("k：%d,v：%c == %d\n", k, v, v)
+		}
+	//*/
 }
 
 func input() {
